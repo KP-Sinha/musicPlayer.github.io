a00112cdfe070727b3c6304a2f007a9581979446
KP-Sinha <kumarprabal1706@gmai.com>
2024-10-09 15:42:32 +0530

    songs.js

---
+++ songs.js
@@ -0,0 +1,44 @@
+// songs.js
+const songs = [
+    {
+        id: 1,
+        name: "Milenge hum nahi",
+        artist: "Artist- Kunnal Verma",
+        img: "images/station/pop.jpg",
+        genre: "Pop",
+        source: "/Img&songs/Milenge Hum Nahi(KoshalWorld.Com).mp3"
+    },
+    {
+        id: 2,
+        name: "Jo Tum Mere Ho",
+        artist: "Artist- Anuv Jain",
+        img: "images/station/rock.jpg",
+        genre: "Rock",
+        source: "/Img&songs/Jo Tum Mere Ho - Anuv Jain 128 Kbps.mp3"
+    },
+    {
+        id: 3,
+        name: "Hale Dil",
+        artist: "Artist- Harshit Saxena",
+        img: "images/station/electronic.jpg",
+        genre: "Jazz",
+        source: "/Img&songs/Hale Dil - Murder 2 128 Kbps.mp3"
+    },
+    {
+        id: 4,
+        name: "Jaanam",
+        artist: "Artist- Vishal Mishra",
+        img: "images/station/party.jpg",
+        genre: "Hip-Hop",
+        source: "/Img&songs/turn-Hip-Hop-195069.mp3"
+    },
+    {
+        id: 5,
+        name: "Prassthanam",
+        artist: "Artist- Dev Negi",
+        img: "images/station/retro.jpg",
+        genre: "Classical",
+        source: "/Img&songs/Prassthanam - Title Track - Prassthanam 128 Kbps.mp3"
+    },
+    // Add more songs as needed
+];
