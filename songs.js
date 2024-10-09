65534bb8c8cf009614c27c933e536d937b24f3b0
KP-Sinha <kumarprabal1706@gmai.com>
2024-10-09 16:10:41 +0530

    changes in songpath

--- songs.js
+++ songs.js
@@ -4,41 +4,41 @@ const songs = [
         id: 1,
         name: "Milenge hum nahi",
         artist: "Artist- Kunnal Verma",
-        img: "images/station/pop.jpg",
+        img: "/musicPlayer.github.io/imgsongs/pop.jpg",
         genre: "Pop",
-        source: "/Img&songs/Milenge Hum Nahi(KoshalWorld.Com).mp3"
+        source: "/musicPlayer.github.io/imgsongs/Milenge Hum Nahi(KoshalWorld.Com).mp3"
     },
     {
         id: 2,
         name: "Jo Tum Mere Ho",
         artist: "Artist- Anuv Jain",
-        img: "images/station/rock.jpg",
+        img: "/musicPlayer.github.io/imgsongs/rock.jpg",
         genre: "Rock",
-        source: "/Img&songs/Jo Tum Mere Ho - Anuv Jain 128 Kbps.mp3"
+        source: "/musicPlayer.github.io/imgsongs/Jo Tum Mere Ho - Anuv Jain 128 Kbps.mp3"
     },
     {
         id: 3,
         name: "Hale Dil",
         artist: "Artist- Harshit Saxena",
-        img: "images/station/electronic.jpg",
+        img: "/musicPlayer.github.io/imgsongs/retro.jpg",
         genre: "Jazz",
-        source: "/Img&songs/Hale Dil - Murder 2 128 Kbps.mp3"
+        source: "/musicPlayer.github.io/imgsongs/Hale Dil - Murder 2 128 Kbps.mp3"
     },
     {
         id: 4,
         name: "Jaanam",
         artist: "Artist- Vishal Mishra",
-        img: "images/station/party.jpg",
+        img: "/musicPlayer.github.io/imgsongs/party.jpg",
         genre: "Hip-Hop",
-        source: "/Img&songs/turn-Hip-Hop-195069.mp3"
+        source: "/musicPlayer.github.io/imgsongs/Jaanam - Bad Newz 128 Kbps.mp3"
     },
     {
         id: 5,
         name: "Prassthanam",
         artist: "Artist- Dev Negi",
-        img: "images/station/retro.jpg",
+        img: "/musicPlayer.github.io/imgsongs/retro.jpg",
         genre: "Classical",
-        source: "/Img&songs/Prassthanam - Title Track - Prassthanam 128 Kbps.mp3"
+        source: "/musicPlayer.github.io/imgsongs/Prassthanam - Title Track - Prassthanam 128 Kbps.mp3"
     },
     // Add more songs as needed
 ];
